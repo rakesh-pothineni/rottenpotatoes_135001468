541bd4dd3a543d23ddf6d6a0074203dec254a1ba
rakesh-pothineni <rakeshpothineni@tamu.edu>
2023-09-26 19:23:06 -0500

    Added 2 new movie details

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,11 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'RRR', :rating => 'PG-13',
+    :release_date => '25-May-2022'},
+  {:title => 'Bahubali The Conclusion', :rating => 'PG-13',
+    :release_date => '22-April-2017 '}
 ]
 
 more_movies.each do |movie|
